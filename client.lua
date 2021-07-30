7814bc889bf5903f5ac5d9940a332903dca7a677
AwakendR34per <86449182+AwakendR34per@users.noreply.github.com>
2021-07-30 12:38:48 -0400

    Initial Build
    Simple wave script for servers with boats.

---
+++ client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+	while true do
+		Wait(0)
+		Citizen.InvokeNative(0xC54A08C85AE4D410, 1.0) --<< You can change this number. 1.0 being smooth as glass.
+	end
+end)
+--wave control by ZakSnapped
